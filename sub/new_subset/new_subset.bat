de0dbbb33c7eae30f683481599a9118c6f79ed2d
adaml <adaml@iri.com>
2021-12-07 14:17:05 -0500

    new location

---
+++ sub/new_subset/new_subset.bat
@@ -0,0 +1,10 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+sortcl /SPECIFICATION=new_subset_0.scl
+sortcl /SPECIFICATION=new_subset_1.scl
+sortcl /SPECIFICATION=new_subset_2.scl
+sortcl /SPECIFICATION=new_subset_3.scl
+sortcl /SPECIFICATION=new_subset_4.scl
+sortcl /SPECIFICATION=new_subset_5.scl
+sortcl /SPECIFICATION=new_subset_6.scl
+echo Job Ended
